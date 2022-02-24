88048bfffaca4ff931bd171a4ae7bc2c0c4a715f
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-02-24 10:46:53 +0800

    Update GCAM_beta2.cpp

--- GCAM_beta2.cpp
+++ GCAM_beta2.cpp
@@ -107,8 +107,9 @@ void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_messag
 		}else{
 			cout<<"SWITCH : error cam_id "<<cam_id<<endl;
 		}
-		data->pipeline = gst_parse_launch(data->current_gst_command, NULL);
-		gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
+		//data->pipeline = gst_parse_launch(data->current_gst_command, NULL);
+		//gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
+		
 	}else if(cap.compare(string("QUIT")) == 0){
 		if(data->streaming_started == true){
 			gst_element_set_state (data->pipeline, GST_STATE_NULL);
